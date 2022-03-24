69032c04bae7dd79b2130190fd3737f7ddba5583
Bartek Mroczkowski <bartlomiej.mroczkowski77@gmail.com>
2022-03-24 15:29:24 +0100

    Next Commit with products list

--- index.js
+++ index.js
@@ -14,17 +14,6 @@ document.querySelectorAll('.navLink').forEach((n) =>
   })
 );
 
-/* White color hover effect on hamburger menu */
-const bar = document.querySelectorAll('.bar');
-
-hamburger.addEventListener('mouseover', () => {
-  bar.forEach((n) => (n.style.backgroundColor = 'white'));
-});
-
-hamburger.addEventListener('mouseleave', () => {
-  bar.forEach((n) => (n.style.backgroundColor = 'black'));
-});
-
 /* Clicking the x clears the input field completely */
 const clearInput = document.querySelector('.fa-xmark');
 const inputField = document.querySelector('.input');
@@ -34,3 +23,55 @@ clearInput.addEventListener('click', () => {
   inputField.focus();
 });
 
+/* Popping up add to cart button */
+
+const addToCart = document.querySelectorAll('.addToCart');
+
+const wholeProduct = document.querySelectorAll('.wholeProduct');
+
+wholeProduct.forEach((n) =>
+  n.addEventListener('mouseenter', () => {
+    addToCart.forEach((n) => n.classList.toggle('active'));
+    wholeProduct.forEach((n) => (n.style.opacity = '0.6'));
+    addToCart.forEach((n) => (n.style.opacity = '1'));
+  })
+);
+
+wholeProduct.forEach((n) =>
+  n.addEventListener('mouseleave', () => {
+    addToCart.forEach((n) => n.classList.toggle('active'));
+    wholeProduct.forEach((n) => (n.style.opacity = '1'));
+    addToCart.forEach((n) => (n.style.opacity = '0'));
+  })
+);
+
+// Renders products to the DOM
+
+const productsEl = document.querySelector('.items');
+
+function renderProducts() {
+  products.forEach((product) => {
+    productsEl.innerHTML += `
+    <li class="wholeProduct">
+          <button class="addToCart">
+            <i class="fa-solid fa-cart-plus"></i>ADD TO CART
+          </button>
+          <div class="product one">
+            <img src="${product.imgSrc}" alt="product 1" />
+            <div class="productName">${product.name}</div>
+            <div class="productPrice">$ ${product.price}</div>
+          </div>
+        </li>
+    `;
+  });
+}
+
+renderProducts();
+
+// Adding to cart
+
+const cart = [];
+
+function addItem(id) {
+  const item = products.find((product) => product.id === id);
+}
